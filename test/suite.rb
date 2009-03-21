f00fae604669d1e3348c115c3bc780ff6f79c7a4
Lou Zell <lzell11@gmail.com>
2009-03-20 22:09:51 -0400

    test suite runs all unit and spec tests

---
+++ test/suite.rb
@@ -0,0 +1,8 @@
+path = File.expand_path(File.dirname(__FILE__))
+
+spec_glob = File.join(path, "**", "*_spec.rb")
+test_glob = File.join(path, "**", "test_*.rb")
+
+tests = Dir.glob(spec_glob) + Dir.glob(test_glob)
+
+tests.each {|test| system("ruby #{test}")}
